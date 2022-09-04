9106f13f5393e1f7236cf287343f7edd9ef3a8a8
Nataliya <Natalia.konyaeva@yandex.ru>
2022-09-04 22:33:14 +0300

    init commit1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите два числа:");
+
+ string first =  System.Console.ReadLine();
+string second =  System.Console.ReadLine();
+ 
+int number1 = int.Parse(first);
+int number2 = int.Parse(second);
+ 
+if (number1 > number2);
+ 
+    {
+ 
+    Console.WriteLine($"{0} наибольшее число", number1);
+ 
+    }
+ 
+    else
+ 
+    {
+ 
+    Console.WriteLine("{0} наибольшее число ", number2);
+ 
+    }
+ 
+    Console.ReadLine();
+
+
